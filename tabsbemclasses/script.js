c4f27d68a87d292be4bb1404cf5c3bce7834d2a7
Andrey Aratov <madingame@yandex.ru>
2017-05-22 13:51:44 +0300

    lil refuctoring

--- tabsbemclasses/script.js
+++ tabsbemclasses/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-    $(".tabs-menu-wrp__item a").click(function(event) {
+    $(".b-tabs-menu__link").click(function(event) {
         event.preventDefault();
-        $(this).parent().addClass("tabs-menu-wrp__item--active");
-        $(this).parent().siblings().removeClass("tabs-menu-wrp__item--active");
-        var tab = $(this).attr("href");
-        $(".tab-content__item").not(tab).css("display", "none");
+        $(this).parent().addClass('b-tabs-menu__item--active');
+        $(this).parent().siblings().removeClass('b-tabs-menu__item--active');
+        var tab = $(this).attr('href');
+        $(".tab-content__item").not(tab).css('display', 'none');
         $(tab).fadeIn();
     });
 });
