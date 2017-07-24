81f91240a899a358c38e276bffcd9af6da6056dc
Andrey Aratov <madingame@yandex.ru>
2017-07-24 21:38:58 +0300

    example bootstrap dots generator

--- bootstrapsliderfix/script.js
+++ bootstrapsliderfix/script.js
@@ -1,10 +1,10 @@
 // Bootsrap carousel dots auto generate
-	$(document).ready(function () {
-	var myCarousel = $(".carousel");
-	var indicators = $(".carousel-indicators"); 
-	myCarousel.find(".carousel-inner").children(".item").each(function(index) {
-		(index === 0) ? 
-		indicators.append("<li data-target='#myCarousel' data-slide-to='"+index+"' class='active'></li>") : 
-		indicators.append("<li data-target='#myCarousel' data-slide-to='"+index+"'></li>");
-	});     
+$(document).ready(function() {
+	var myCarousel = $('.carousel');
+	var indicators = $('.carousel-indicators');
+	myCarousel.find('.carousel-inner').children('.item').each(function(index) {
+		(index === 0) ?
+		indicators.append('<li data-target="#carousel-example-generic" data-slide-to="' + index + '" class="active"></li>'):
+		indicators.append('<li data-target="#carousel-example-generic" data-slide-to="' + index + '"></li>');
+	});
 });
